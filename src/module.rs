1e900d2d3905c8a74f1c084f8b092b59e0ed8228
chronium <chronium@users.noreply.github.com>
2020-03-01 15:41:54 +0200

    Add basic global ops

--- src/module.rs
+++ src/module.rs
@@ -2,18 +2,18 @@
 
 extern crate llvm_sys;
 
+use self::llvm_sys::analysis::{LLVMVerifierFailureAction, LLVMVerifyModule};
 use self::llvm_sys::core::*;
 use self::llvm_sys::prelude::*;
-use self::llvm_sys::analysis::{LLVMVerifyModule, LLVMVerifierFailureAction};
 use cstring_manager::CStringManager;
+use engine::Engine;
+use function;
 use std::ffi::CString;
 use std::os::raw::c_char;
-use function;
-use engine::Engine;
 
 #[derive(Debug)]
 pub struct Module {
-    llvm_module: LLVMModuleRef
+    llvm_module: LLVMModuleRef,
 }
 
 impl Module {
@@ -21,7 +21,7 @@ impl Module {
         let mod_name_ptr = CStringManager::new_cstring_as_ptr(name);
         let module = unsafe { LLVMModuleCreateWithName(mod_name_ptr) };
         Module {
-            llvm_module: module
+            llvm_module: module,
         }
     }
 
@@ -29,7 +29,7 @@ impl Module {
         let mod_name_ptr = CStringManager::new_cstring_as_ptr(name);
         let module = unsafe { LLVMModuleCreateWithNameInContext(mod_name_ptr, context) };
         Module {
-            llvm_module: module
+            llvm_module: module,
         }
     }
 
@@ -48,39 +48,62 @@ impl Module {
         function::Function::from_ptr(named_function)
     }
 
-    pub fn get_or_add_function(&self, name: &str, function_type: LLVMTypeRef) -> function::Function {
+    pub fn get_or_add_function(
+        &self,
+        name: &str,
+        function_type: LLVMTypeRef,
+    ) -> function::Function {
         let func_name_ptr = CStringManager::new_cstring_as_ptr(name);
         let named_function = unsafe { LLVMGetNamedFunction(self.llvm_module, func_name_ptr) };
         if named_function.is_null() {
             function::Function::new(self.llvm_module, name, function_type)
-        }else{
+        } else {
             function::Function::from_ptr(named_function)
         }
+    }
+
+    #[inline]
+    pub fn add_global(&self, ty: LLVMTypeRef, name: &str) -> LLVMValueRef {
+        let glob_name_ptr = CStringManager::new_cstring_as_ptr(name);
+        unsafe { LLVMAddGlobal(self.as_ref(), ty, glob_name_ptr) }
+    }
+
+    #[inline]
+    pub fn set_initializer(&self, ptr: LLVMValueRef, val: LLVMValueRef) {
+        unsafe { LLVMSetInitializer(ptr, val) }
     }
 
     pub fn verify(&self) -> Result<(), String> {
         let mut error: *mut c_char = 0 as *mut c_char;
         let ok = unsafe {
             let buf: *mut *mut c_char = &mut error;
-            LLVMVerifyModule(self.llvm_module, LLVMVerifierFailureAction::LLVMReturnStatusAction, buf)
+            LLVMVerifyModule(
+                self.llvm_module,
+                LLVMVerifierFailureAction::LLVMReturnStatusAction,
+                buf,
+            )
         };
-        if ok == 1 { // error
+        if ok == 1 {
+            // error
             let err_msg = unsafe { CString::from_raw(error).into_string().unwrap() };
             Err(err_msg)
-        }else{ // success
+        } else {
+            // success
             Ok(())
         }
     }
 
     #[inline]
-    pub fn dump(&self){
+    pub fn dump(&self) {
         unsafe { LLVMDumpModule(self.llvm_module) }
     }
 
     pub fn print_module_to_string(&self) -> String {
         let ptr = unsafe { LLVMPrintModuleToString(self.llvm_module) };
         let string = unsafe { CString::from_raw(ptr).into_string().unwrap() };
-        unsafe { LLVMDisposeMessage(ptr); }
+        unsafe {
+            LLVMDisposeMessage(ptr);
+        }
         string
     }
 
@@ -91,11 +114,13 @@ impl Module {
             let buf: *mut *mut c_char = &mut error;
             LLVMPrintModuleToFile(self.llvm_module, fname_ptr, buf)
         };
-        if ok == 1 { // error
+        if ok == 1 {
+            // error
             let err_msg = unsafe { CString::from_raw(error).into_string().unwrap() };
             unsafe { LLVMDisposeMessage(error) }
             Err(err_msg)
-        }else{ // success
+        } else {
+            // success
             Ok(())
         }
     }
